f22c25301c0d74e3e026270b1f138445b49d2e90
defrindr <defrindr@gmail.com>
2019-10-09 20:35:49 +0700

    add button print member card , and remove class table responsive

--- resources/views/admin/costumer/show.blade.php
+++ resources/views/admin/costumer/show.blade.php
@@ -11,37 +11,38 @@
 @section('content')
     <div class="box box-primary">
         <div class="box-header">
-        	<a src="{{ route('costumer_index') }}" onclick="goto(this);return 0;" class="btn btn-danger btn-lg text-white">Back to index</a>
+			<a src="{{ route('costumer_index') }}" onclick="goto(this);return 0;" class="btn btn-danger btn-lg text-white">Back to index</a>
+			<a target="blank" href="{{ route('costumer_cetak',$costumer->id) }}" class="btn btn-primary btn-lg text-white">Cetak Kartu Anggota</a>
         </div>
         <div class="box-body">
-        	<table class="table table-bordered table-stripped table-responsive">
-        		<tbody>
-        			<tr>
-        				<td>ID Anggota</td><td>:</td><td>{{ $costumer->id }}</td>
-        			</tr>
-        			<tr>
-        				<td>Nama</td><td>:</td><td>{{ $costumer->nama }}</td>
-        			</tr>
-        			<tr>
-        				<td>Email</td><td>:</td><td>{{ $costumer->email }}</td>
-        			</tr>
-        			<tr>
-        				<td>No KTP</td><td>:</td><td>{{ $costumer->no_ktp }}</td>
-        			</tr>
-        			<tr>
-        				<td>No HP</td><td>:</td><td>{{ $costumer->no_hp }}</td>
-        			</tr>
-        			<tr>
-        				<td>Nama</td><td>:</td><td>{{ $costumer->nama }}</td>
-        			</tr>
-        			<tr>
-        				<td>Alamat</td><td>:</td><td>{{ $costumer->alamat }}</td>
-        			</tr>
-        			<tr>
-        				<td>Terdaftar pada</td><td>:</td><td>{{ date('H:i A / d F Y',strtotime($costumer->created_at)) }}</td>
-        			</tr>
-        		</tbody>
-        	</table>
+			<table class="table table-bordered table-stripped">
+				<tbody>
+					<tr>
+						<td>ID Anggota</td><td>:</td><td>{{ $costumer->id }}</td>
+					</tr>
+					<tr>
+						<td>Nama</td><td>:</td><td>{{ $costumer->nama }}</td>
+					</tr>
+					<tr>
+						<td>Email</td><td>:</td><td>{{ $costumer->email }}</td>
+					</tr>
+					<tr>
+						<td>No KTP</td><td>:</td><td>{{ $costumer->no_ktp }}</td>
+					</tr>
+					<tr>
+						<td>No HP</td><td>:</td><td>{{ $costumer->no_hp }}</td>
+					</tr>
+					<tr>
+						<td>Nama</td><td>:</td><td>{{ $costumer->nama }}</td>
+					</tr>
+					<tr>
+						<td>Alamat</td><td>:</td><td>{{ $costumer->alamat }}</td>
+					</tr>
+					<tr>
+						<td>Terdaftar pada</td><td>:</td><td>{{ date('H:i A / d F Y',strtotime($costumer->created_at)) }}</td>
+					</tr>
+				</tbody>
+			</table>
         </div>
     </div>
 @endsection
