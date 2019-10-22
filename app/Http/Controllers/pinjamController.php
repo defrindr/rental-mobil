ae31688c9235c6152448a4e8d8d8d93290fa0643
defrindr <defrindr@gmail.com>
2019-10-22 15:54:28 +0700

    Updated pinjamController.php , fix error when update data

--- app/Http/Controllers/pinjamController.php
+++ app/Http/Controllers/pinjamController.php
@@ -20,30 +20,25 @@ class pinjamController extends Controller
         if($request->q != "") $q = $request->q;
         if($request->year != "") $year = $request->year;
         if($request->month != "") $month = $request->month;
-        // if(MyHelper::isAdmin())
-        // {
-            $pinjam = pinjamModel::select('pinjam_module.*')
-                        ->cari($q)
-                        ->time($year,$month)
-                        ->Where([
-                            'pinjam_module.created_by' => MyHelper::id()])
-                        ->orderBy('pinjam_module.created_at','desc')
-                        ->paginate(5);
-        // }
-        // else
-        // {
-        //     $pinjam = pinjamModel::select('pinjam_module.*')
-        //                 ->cari($q)
-        //                 ->Where([
-        //                     'pinjam_module.created_by' => MyHelper::id()])
-        //                 ->orderBy('pinjam_module.created_at','desc')
-        //                 ->paginate(5);
-        // }
-        return view('admin.pinjam.index',['pinjams' => $pinjam]);
+        //
+        $pinjam = pinjamModel::select('pinjam_module.*')
+                    ->cari($q)
+                    ->time($year,$month)
+                    ->Where([
+                        'pinjam_module.created_by' => MyHelper::id()])
+                    ->orderBy('pinjam_module.created_at','desc')
+                    ->paginate(5);
+        return view('admin.pinjam.index',[
+                                'pinjams' => $pinjam]);
     }
 
 
-
+    /**
+     * Show form create pinjam
+     * 
+     * @param
+     * @return view
+     */
     public function create()
     {
         $costumers = costumerModel::orderBy('nama')
@@ -51,19 +46,22 @@ class pinjamController extends Controller
         $mobils = mobilModel::Where('status','=','1')
                         ->orderBy('merk')
                         ->get();
-
         return view('admin.pinjam.create',[
                             'costumers' => $costumers
                             ,'mobils' => $mobils]);
     }
 
 
-
+    /**
+     * Created new pinjam data
+     * 
+     * @param $request
+     * @return redirect
+     */
     public function store(Request $request)
     {
         $pinjam = new pinjamModel();
         $mobil = mobilModel::findOrFail($request->id_mobil);
-
         $request->merge([
                         'tanggal_pinjam'  =>  date('Y-m-d H:i:s',strtotime($request->tanggal_pinjam))]);
         $request->request->add([
@@ -72,28 +70,39 @@ class pinjamController extends Controller
                         'status' => 0,
                         'total' => 0,
                         'updated_at' => date('Y-m-d H:i:s',time())]);
-
+        //
         if($mobil->status == 1)
         {
             $mobil->status = 0;
             $pinjam->fill($request->all());
-
-            $mobil->save();
-            $pinjam->save();
-
-            return redirect()
+            try{
+                DB::beginTransaction();
+                $mobil->save();
+                $pinjam->save();
+                DB::commit();
+                return redirect()
                     ->route('pinjam_index')
-                    ->with('success','Success added data');
+                    ->with('success', 'Success created data.');
+            }catch(\Exception $e){
+                return redirect()
+                    ->route('pinjam_index')
+                    ->with('error', 'Error when creating data.');
+            }
         }
         else
         {
             return redirect()
                     ->route('pinjam_index')
-                    ->with('error','Mobil Kosong');
+                    ->with('error','Mobil tidak tersedia.');
         }
     }
 
-
+    /**
+     * Show form update pinjam
+     * 
+     * @param $id
+     * @return view
+     */
     public function edit($id)
     {
         $pinjam = pinjamModel::findOrFail($id);
@@ -117,9 +126,14 @@ class pinjamController extends Controller
         $pinjam = pinjamModel::findOrFail($id);
         $mobil = mobilModel::findOrFail($pinjam->id_mobil);
 
+        if($request->tanggal_pinjam == null){
+            $request->tanggal_pinjam = $pinjam->tanggal_pinjam;
+        }else{
+            $request->merge(['tanggal_pinjam' => date('Y-m-d H:i:s',strtotime($request->tanggal_pinjam))]);
+        }
+
         $request->merge([
                     'status' => (($request->status=="on") ? 1 : 0),
-                    'tanggal_pinjam' => date('Y-m-d H:i:s',strtotime($request->tanggal_pinjam)),
                     'tanggal_kembali' => date('Y-m-d H:i:s',strtotime($request->tanggal_kembali))]);
         $request->request->add([
                     'updated_at' => date('Y-m-d H:i:s',time())]);
@@ -132,7 +146,7 @@ class pinjamController extends Controller
             {
                 return redirect()
                         ->route('pinjam_index')
-                        ->with('error','Tanggal kembali less than tanggal pinjam');
+                        ->with('error','Tanggal kembali less than tanggal pinjam.');
             }
             else{
                 //Counting Fee
@@ -140,12 +154,24 @@ class pinjamController extends Controller
                 $total_harga = (int)$lama_pinjam*$pinjam->mobil->harga;
                 $request->request->add([
                             'total' => $total_harga]);
-
-                $pinjam->update($request->all());
-                $mobil->save();
-                return redirect()
+                //
+                try
+                {
+                    DB::beginTransaction();
+                    $pinjam->update($request->all());
+                    $mobil->save();
+                    DB::commit();
+                    return redirect()
+                            ->route('pinjam_index')
+                            ->with('success','Success updated data.');
+                }
+                catch(\Exception $e)
+                {
+                    DB::rollback();
+                    return redirect()
                         ->route('pinjam_index')
-                        ->with('success','Success updated data');
+                        ->with('error', 'Error 1 when updating data.');
+                }
             }
         }
         else if( $request->status == 0) // status masih dipinjam
@@ -155,13 +181,21 @@ class pinjamController extends Controller
                         'tanggal_kembali' => null]);
             $request->request->add([
                         'total' => 0]);
-            $pinjam->update($request->all());
-            $mobil->save();
-            return redirect()
+            try {
+                DB::beginTransaction();
+                $pinjam->update($request->all());
+                $mobil->save();
+                DB::commit();
+                return redirect()
                     ->route('pinjam_index')
-                    ->with('success','Success updated data');
+                    ->with('success', 'Success updated data.');
+            } catch (\Exception $e) {
+                DB::rollback();
+                return redirect()
+                    ->route('pinjam_index')
+                    ->with('error', 'Error 2 when updating data.'.$e);
+            }
         }
-
     }
 
 
@@ -169,11 +203,16 @@ class pinjamController extends Controller
     public function destroy($id)
     {
         $pinjam = pinjamModel::findOrFail($id);
-        $pinjam->delete();
-
-        return redirect()
+        if($pinjam->delete()) {
+            return redirect()
                 ->route('pinjam_index')
-                ->with('success','Success deleted data');
+                ->with('success', 'Success deleted data.');
+        }
+        else {
+            return redirect()
+                ->route('pinjam_index')
+                ->with('error', 'Error when deleting data.');
+        }
     }
 
 
@@ -182,27 +221,37 @@ class pinjamController extends Controller
     {
         $pinjam = pinjamModel::findOrFail($id);
         $mobil = mobilModel::findOrFail($pinjam->id_mobil);
-
-        if($pinjam->status == 1)
+        if($pinjam->status == 1) // Telah dikembalikan
         {
             return redirect()
                     ->route('pinjam_index')
-                    ->with('error','Pinjam already completed');
+                    ->with('error','Pinjam already completed.');
         }
         else
         {
             $pinjam->tanggal_kembali = date('Y-m-d H:i:s',time());
             if(strtotime($pinjam->tanggal_kembali) > strtotime($pinjam->tanggal_pinjam))
             {
-                $pinjam->status = 1; // status dikembalikan
+                $pinjam->status = 1; // Status telah dikembalikan
                 $pinjam->total = (int)((strtotime($pinjam->tanggal_kembali)-strtotime($pinjam->tanggal_pinjam))/(60*60)*$pinjam->mobil->harga);
-                $mobil->status = 1; // mobil tersedia
-
-                $mobil->save();
-                $pinjam->save();
-                return redirect()
+                $mobil->status = 1; // Mobil kembali tersedia
+                try
+                {
+                    DB::beginTransaction(); // begin transaction
+                    $mobil->save();
+                    $pinjam->save();
+                    DB::commit(); // comiit transaction
+                    return redirect()
                         ->route('pinjam_index')
-                        ->with('success','Success changed data');
+                        ->with('success', 'Success changed data.');
+                }
+                catch(\Exception $e)
+                {
+                    DB::rollBack(); // rollback transaction
+                    return redirect()
+                        ->route('pinjam_index')
+                        ->with('error', 'Error when changing data');
+                }
             }
             else
             {
@@ -214,67 +263,71 @@ class pinjamController extends Controller
     }
 
     /**
-     * cetakAll
-     * Mencetak Semua data pinjam pada bulan ini
+     * Generate report pinjam in one month
+     * 
+     * @param $request
+     * @return $pdf
      */
     public function cetakAll(Request $request)
     {
-        $bulan = array (1 =>   'Januari',
-				'Februari',
-				'Maret',
-				'April',
-				'Mei',
-				'Juni',
-				'Juli',
-				'Agustus',
-				'September',
-				'Oktober',
-				'November',
-				'Desember'
-			);
-        $q = "";
-        $year = date('Y',time());
-        $month = (int)date('m',time());
-        if($request->q != "") $q = $request->q;
-        if($request->year != "") $year = $request->year;
-        if($request->month != "") $month = $request->month;
-
-
-        $title = "Laporan pemasukan bulan ".$bulan[$month]." ".$year;
-        $pinjams = pinjamModel::Where(['created_by' => MyHelper::id()])
-                                    ->cari($q)
-                                    ->time($year,$month)
-                                    ->orderBy('pinjam_module.created_at','desc')
-                                    ->get();
-        $totalPendapatan = pinjamModel::Where(['created_by' => MyHelper::id()])
-                                    ->cari($q)
-                                    ->time($year,$month)
-                                    ->select(DB::raw("sum(total) as totalPendapatan"))->get();
-
-        $pdf = \PDF::loadview('admin.pinjam.template.layout',[
-                        'title' => $title
-                        ,'pinjams' => $pinjams
-                        ,'totalPendapatan' => $totalPendapatan[0]->totalPendapatan])
-                    ->setPaper('f4', 'landscape')
-                    ->stream($title.$month.".pdf", array("Attachment" => false));
-        return $pdf;
+        try {
+            $bulan = array(
+                1 =>   'Januari',
+                'Februari',
+                'Maret',
+                'April',
+                'Mei',
+                'Juni',
+                'Juli',
+                'Agustus',
+                'September',
+                'Oktober',
+                'November',
+                'Desember'
+            );
+            $q = "";
+            $year = date('Y', time());
+            $month = (int) date('m', time());
+            if ($request->q != "") $q = ($request->q);
+            if ($request->year != "") $year = $request->year;
+            if ($request->month != "") $month = $request->month;
+            //
+            $title = "Laporan pemasukan bulan " . $bulan[$month] . " " . $year;
+            $pinjams = pinjamModel::Where(['created_by' => MyHelper::id()])
+                ->cari($q)
+                ->time($year, $month)
+                ->orderBy('pinjam_module.created_at', 'desc')
+                ->get();
+            $totalPendapatan = pinjamModel::Where(['created_by' => MyHelper::id()])
+                ->cari($q)
+                ->time($year, $month)
+                ->select(DB::raw("sum(total) as totalPendapatan"))->get();
+            $pdf = \PDF::loadview('admin.pinjam.template.layout', [
+                'title' => $title, 'pinjams' => $pinjams, 'totalPendapatan' => $totalPendapatan[0]->totalPendapatan
+            ])
+                ->setPaper('f4', 'landscape')
+                ->stream($title . $month . ".pdf", array("Attachment" => false));
+            return $pdf;
+        }catch(\Exception $e){
+            abort(500, 'Goblok. Server error mz.');
+        }
     }
 
-
-    public function cetakStruct($id){
+    /**
+     * Created struct pinjam in pdf format
+     * 
+     * @param $id
+     * @return $pdf
+     */
+    public function cetakStruct($id)
+    {
         $pinjam = pinjamModel::findOrFail($id);
         $title = "Struct_peminjaman_".str_replace(" ","_",$pinjam->costumer->nama).strtotime($pinjam->tanggal_pinjam)."_".strtotime($pinjam->tanggal_kembali);
         $pdf = \PDF::loadview('admin.pinjam.template.struct_pinjam',[
             'pinjam' => $pinjam
             ,'title' => $title])
-        ->setPaper('f4', 'landscape')
+        ->setPaper([0,0,480,350], 'landscape')
         ->stream($title.".pdf", array("Attachment" => false));
         return $pdf;
-
-        // return view('admin.pinjam.template.struct_pinjam',[
-        //     "pinjam" => $pinjam
-        //     ,"title" => $title
-        // ]);
     }
-
 }
