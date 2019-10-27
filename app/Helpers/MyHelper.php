8a08a256d26dcd7807cbca53556e06c0a5b50b45
defrindr <defrindr@gmail.com>
2019-10-27 13:26:25 +0700

    add function getTimeNow and getUserName

--- app/Helpers/MyHelper.php
+++ app/Helpers/MyHelper.php
@@ -2,6 +2,8 @@
 
 namespace App\Helpers;
 
+use App\Models\adminModel;
+
 class MyHelper
 {
 	/**
@@ -42,6 +44,32 @@ class MyHelper
 
 	public static function setDate(string $date){
 		return date('H:i A d F Y',strtotime($date));
+	}
+
+
+	public static function getTimeNow(){
+		$exp = explode(' ',date('H:i A d F Y',time()));
+		$listMonth = array(
+                1 =>   'Januari',
+                'Februari',
+                'Maret',
+                'April',
+                'Mei',
+                'Juni',
+                'Juli',
+                'Agustus',
+                'September',
+                'Oktober',
+                'November',
+                'Desember'
+			);
+		$monthNow = date('m',time());
+		$exp[3] = $listMonth[$monthNow];
+		return join(" ",$exp);
+	}
+
+	public static function getUserName(){
+		return adminModel::Where(['id'=>self::id()])->select("username")->get()[0]['username'];
 	}
 
 }
